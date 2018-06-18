f7b3d496d9275e926c6b122d4d2ab83964b1b757
Duncan <loxa10077@gmail.com>
2018-06-18 12:09:12 +0800

    add authenticate_admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Get out of here"
+      redirect_to root_path
+    end
+  end
 end

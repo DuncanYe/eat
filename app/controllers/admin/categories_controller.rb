08965e392268425eb74a2719f71457d2bb58c428
Duncan <loxa10077@gmail.com>
2018-06-26 09:50:25 +0800

    admin can create new category on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,27 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @category = Category.new
   end
-  
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "成功建立新分類"
+      redirect_to admin_categories_path
+    else
+      flash[:alert] = "分類建立有誤"
+      @categories = Category.all
+      render :index
+      # render 會直接從Create action 直接去找view，不會經過index action
+      # 所以要在給 @categories = Category.all
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end

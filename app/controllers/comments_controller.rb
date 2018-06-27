36f89701a11b73f1be716340684f37faf63a6990
Duncan <loxa10077@gmail.com>
2018-06-27 15:00:15 +0800

    user can add comment on restaurant show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = @restaurant.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      redirect_to request.referrer, alert: "Comment不得為空白"
+      # redirect_to restaurant_path(@restaurant), alert: "Comment不得為空白"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end

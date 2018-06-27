7d4dee344c5df8a4a6f8a40c4e045dfb09cdfddc
Duncan <loxa10077@gmail.com>
2018-06-27 15:31:37 +0800

    admin can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_restaurant
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = @restaurant.comments.build(comment_params)
     @comment.user = current_user
     if @comment.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: "留言成功"
     else
       redirect_to request.referrer, alert: "Comment不得為空白"
       # redirect_to restaurant_path(@restaurant), alert: "Comment不得為空白"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if current_user.admin?
+      @comment.destroy
+      redirect_to restaurant_path(@restaurant), alert: "留言已刪除"
     end
   end
 
@@ -16,6 +24,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
 end

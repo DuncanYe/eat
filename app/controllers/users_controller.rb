8e7b9709f9a83278babbad68ef71505042f6d67a
Duncan <loxa10077@gmail.com>
2018-06-28 09:47:03 +0800

    user can edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
 
 
   def show
-    @user = current_user
   end
-  
+
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :avatar, :intro)
+  end
+
 end

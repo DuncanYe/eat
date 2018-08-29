0623096196214e61f7a8939f1f9e9a862a380467
Duncan <loxa10077@gmail.com>
2018-08-29 12:08:33 +0800

    create add friend button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,7 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    current_user.friendships.create!(friend_id: params[:friend_id])
+    redirect_back(fallback_location: root_path)
+  end
 end

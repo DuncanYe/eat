326a529b81e913195f3cf4f152187e0573e6823c
Duncan <loxa10077@gmail.com>
2018-08-29 11:54:38 +0800

    setup relation between user and friendship table

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,7 @@ class User < ApplicationRecord
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
 end

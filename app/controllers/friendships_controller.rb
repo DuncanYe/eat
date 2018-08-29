485099b55f0ecb26581656acde9b3efce4d69aac
Duncan <loxa10077@gmail.com>
2018-08-29 12:21:57 +0800

    add flash messages when user add friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,12 @@
 class FriendshipsController < ApplicationController
 
   def create
-    current_user.friendships.create!(friend_id: params[:friend_id])
+     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "加好友了！"
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence 
+    end
     redirect_back(fallback_location: root_path)
   end
 end

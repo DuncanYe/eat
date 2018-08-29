c1a76e4ce7920f9123a2a0654dab91ec60926de3
Duncan <loxa10077@gmail.com>
2018-08-29 11:57:18 +0800

    add friendship routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :friendships, only: [:create, :destroy]
+
   namespace :admin do
     resources :restaurants
     resources :categories
